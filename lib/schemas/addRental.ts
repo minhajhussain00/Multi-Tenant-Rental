b442d35296725411ebba2514b7cf2967582b4b8d
Minhaj Mehboob Hussain <146344300+minhajhussain00@users.noreply.github.com>
2025-10-19 12:12:41 +0500

    Revert "haaaaaaaaaaaaaaaaa"

--- lib/schemas/addRental.ts
+++ lib/schemas/addRental.ts
@@ -1,12 +1,10 @@
 import { z } from "zod";
-import { id } from "zod/v4/locales";
 
 export const addRentalSchema = z.object({
-  id:z.string(),
   rental_name: z.string().min(2, "Name is required"),
   rental_description: z.string().min(10, "Description must be at least 10 characters"),
   price: z.number().min(1, "Price must be greater than 0"),
-  image: z.string().min(1, "Image is required")
+  image: z.instanceof(File).optional(),
 });
 
 export type AddRentalSchema = z.infer<typeof addRentalSchema>;
