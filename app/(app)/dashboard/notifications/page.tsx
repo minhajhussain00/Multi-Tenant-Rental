5cf3bfec0ffc3e919548744c5bd567547c079ce0
Minhaj Mehboob <minhajhussain359@gmail.com>
2025-12-27 21:39:43 +0500

    feat: add placeholder pages for notifications and settings

---
+++ app/(app)/dashboard/notifications/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-gray-600 text-4xl text-center'>
+      Coming soon...
+    </div>
+  )
+}
+
+export default page
